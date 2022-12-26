c8b2da3ab7b6b7f04abb02f230575e912275afeb
watcher041 <watcher041@gmail.com>
2022-12-27 08:35:31 +0900

    コンパイルできるところまで追記

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
 
-/*
-    以下のコマンドを実行してコンパイルするとエラーが発生しない。
-    rustup target add thumbv7em-none-eabihf
-    cargo build --target thumbv7em-none-eabihf
-*/
-
 #![no_std] // Rust の標準ライブラリにリンクしない
 
 #![no_main] // 全ての Rust レベルのエントリポイントを無効にする
@@ -12,7 +6,7 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle] // この関数の名前修飾をしない
+#[no_mangle] // この関数の名前修飾をしない（他の_start関数を探さないようにする）
 pub extern "C" fn _start() -> ! {
     // リンカはデフォルトで `_start` という名前の関数を探すので、
     // この関数がエントリポイントとなる
@@ -20,6 +14,7 @@ pub extern "C" fn _start() -> ! {
 }
 
 /// この関数はパニック時に呼ばれる
+// パニック時の動作を記述しないとコンパイルエラーになる
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
